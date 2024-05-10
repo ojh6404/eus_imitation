e410d806d9bcf0db702e4d7f2e2bc251d53eac85
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-05-11 00:49:59 +0900

    docker setup

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
 sudo apt install -y python3.9 python3.9-dev python3.9-venv
+git clone https://github.com/ojh6404/octo.git
 python3.9 -m pip install -U setuptools wheel pip
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -e octo
+python3.9 -m pip install -r octo/requirements.txt
 python3.9 -m pip install jax[cuda11_pip]==0.4.20 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-python3.9 -m pip install flax==0.7.5 optax==0.1.5 orbax
+python3.9 -m pip install scipy<1.13.0
+# python3.9 -m pip install flax==0.7.5 optax==0.1.5
 # python3.9 -m pip install tensorflow[and-cuda]
