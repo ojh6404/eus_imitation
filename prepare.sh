182dda37261824bb69b4747cf09d5d5fd1a10c2c
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-05-10 23:04:18 +0900

    docker setup

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,6 @@
 
 sudo apt install -y python3.9 python3.9-dev python3.9-venv
 python3.9 -m pip install -U setuptools wheel pip
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements_octo.txt
+python3.9 -m pip install jax[cuda12_pip]==0.4.23 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+python3.9 -m pip install jaxlib==0.4.23 flax==0.7.5 optax==0.1.5 orbax
