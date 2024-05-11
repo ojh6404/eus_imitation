1717d534db87e61aa6c90cb9704dcf5346f2d11c
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-05-11 15:35:35 +0900

    docker setup

--- prepare.sh
+++ prepare.sh
@@ -7,5 +7,3 @@ python3.9 -m pip install -e octo
 python3.9 -m pip install -r octo/requirements.txt
 python3.9 -m pip install jax[cuda11_pip]==0.4.20 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 python3.9 -m pip install scipy==1.12.0 omegaconf
-# python3.9 -m pip install flax==0.7.5 optax==0.1.5
-# python3.9 -m pip install tensorflow[and-cuda]
