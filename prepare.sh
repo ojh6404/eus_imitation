2eadd840ead303e2d085ec8e924b67d2eb19de12
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-05-11 22:02:14 +0900

    docker setup

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,8 @@
 
 sudo apt install -y python3.9 python3.9-dev python3.9-venv
 git clone https://github.com/ojh6404/octo.git
-python3.9 -m pip install -U setuptools wheel pip
+python3.9 -m pip install -U wheel pip
+python3.9 -m pip install setuptools==59.5.0
 python3.9 -m pip install -e octo
 python3.9 -m pip install -r octo/requirements.txt
 python3.9 -m pip install jax[cuda11_pip]==0.4.20 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
