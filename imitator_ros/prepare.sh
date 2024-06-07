bea5b393b71e8c89c06e8a7d462a8f38be591e0d
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-06-07 15:30:49 +0900

    update

--- imitator_ros/prepare.sh
+++ imitator_ros/prepare.sh
@@ -4,4 +4,5 @@ git clone https://github.com/ojh6404/imitator.git ~/imitator
 git clone https://github.com/ojh6404/octo.git ~/octo
 python3.11 -m pip install -e ~/imitator[cuda]
 python3.11 -m pip install -e ~/octo
-python3.11 -m pip install --upgrade "jax[cuda11_pip]==0.4.25" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# python3.11 -m pip install --upgrade "jax[cuda11_pip]==0.4.25" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+python3.11 -m pip install -U "jax[cuda12]"
