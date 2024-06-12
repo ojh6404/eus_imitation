b1cf8d7c0781d62882774482cace26ba8ae243f9
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-06-12 18:36:27 +0900

    fix jax version

--- imitator_ros/prepare.sh
+++ imitator_ros/prepare.sh
@@ -4,4 +4,4 @@ git clone https://github.com/ojh6404/imitator.git ~/imitator
 git clone https://github.com/ojh6404/octo.git ~/octo
 python3.11 -m pip install -e ~/imitator[cuda]
 python3.11 -m pip install -e ~/octo
-python3.11 -m pip install -U "jax[cuda12]"
+python3.11 -m pip install --upgrade "jax[cuda12]==0.4.28" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
